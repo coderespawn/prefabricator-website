f8ff007d72e502dcb1a5c80b0c9fb9ab2b048e09
Ali Akbar <ali.akbar@coderespawn.com>
2019-01-02 20:59:52 +0530

    Updated the download url

--- js/download.js
+++ js/download.js
@@ -1,7 +1,7 @@
 function HandleDownload(version) {
 	var platform = $("input[name='BuildPlatform']:checked").val();
 	if(platform){
-		var url = "https://github.com/coderespawn/prefabricator-ue4/releases/download/" + version + "/Binaries-" + platform + "-Prefabricator.zip";
+		var url = "https://github.com/coderespawn/prefabricator-ue4/releases/download/v" + version + "/Binaries-" + platform + "-Prefabricator.zip";
 		window.location = url;
 	}
 }
